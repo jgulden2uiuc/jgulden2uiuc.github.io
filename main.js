478fe958502470ddfbee9192ad789799d419008b
jgulden2uiuc <140918950+jgulden2uiuc@users.noreply.github.com>
2023-07-29 18:35:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -100,13 +100,16 @@ d3.csv("API_NY.GDP.PCAP.KD_DS2_en_csv_v2_5728900.csv").then(function(data) {
 
 
   // Draw legend
-  country.append("text")
-    .datum(([key, values]) => ({id: key, value: values[values.length - 1]}))
-    .attr("transform", function(d) { return "translate(" + xScale(d.value.year) + "," + yScale(d.value.gdp) + ")"; })
-    .attr("x", 3)
-    .attr("dy", ".35em")
-    .style("font", "10px sans-serif")
-    .text(function(d) { return d.id; });
+  svg.selectAll(".country")
+    .data(dataByCountry)
+    .enter().append("text")
+      .attr("class", "legend")
+      .datum(function([key, values]) { return {id: key, value: values[values.length - 1]}; })
+      .attr("transform", function(d) { return "translate(" + xScale(d.value.year) + "," + yScale(d.value.gdp) + ")"; })
+      .attr("x", 3)
+      .attr("dy", "0.35em")
+      .style("font", "10px sans-serif")
+      .text(function(d) { return d.id; });
 });
 
 // Back button functionality
