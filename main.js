ef58ae30b4ab514039ea5fab493505c411adbdb5
jgulden2uiuc <140918950+jgulden2uiuc@users.noreply.github.com>
2023-07-29 19:00:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// main.js
+
 // Width and height of the chart
 const width = 960, height = 500;
 const margin = {top: 20, right: 80, bottom: 30, left: 50};
@@ -26,7 +28,7 @@ const line = d3.line()
   .y(function(d) { return yScale(d.gdp); });
 
 // Load data
-d3.csv("API_NY.GDP.PCAP.KD_DS2_en_csv_v2_5728900.csv").then(function(data) {
+d3.csv("data.csv").then(function(data) {
   // Prepare the data
   const parsedData = [];
   data.forEach(d => {
@@ -40,11 +42,10 @@ d3.csv("API_NY.GDP.PCAP.KD_DS2_en_csv_v2_5728900.csv").then(function(data) {
   });
 
   // Group data by country
-  const dataByCountry = d3.group(parsedData, d => d.country);
-  const countries = Array.from(dataByCountry.keys());
-
-  color.domain(countries);
-
+  const dataByCountry = Array.from(d3.group(parsedData, d => d.country));
+  
+  color.domain(dataByCountry.map(([key, values]) => key));
+  
   // Set domains for scales
   xScale.domain(d3.extent(parsedData, function(d) { return d.year; }));
   yScale.domain([0, d3.max(parsedData, function(d) { return d.gdp; })]);
@@ -61,55 +62,23 @@ d3.csv("API_NY.GDP.PCAP.KD_DS2_en_csv_v2_5728900.csv").then(function(data) {
 
   // Draw lines
   const country = svg.selectAll(".country")
-    .data(Array.from(dataByCountry))
+    .data(dataByCountry)
     .enter().append("g")
       .attr("class", "country");
-  
+
   country.append("path")
     .attr("class", "line")
     .attr("d", ([key, values]) => line(values))
-    .style("stroke", ([key, values]) => color(key))
-    .on('click', function(event, [key, values]) {
-        // Update scales
-        yScale.domain([0, d3.max(values, d => d.gdp)]);
-        xScale.domain(d3.extent(values, d => d.year));
-  
-        // Update axes
-        svg.select('.x.axis').transition().call(xAxis);
-        svg.select('.y.axis').transition().call(yAxis);
-  
-        // Redraw line
-        d3.select(this).attr("d", line(values));
-  
-        // Show back button
-        d3.select("#back").style("visibility", "visible");
-    })
-    .on('mouseover', function() {
-        d3.selectAll('.line')
-          .style('opacity', 0.2)
-          .style('stroke-width', '1px');
-        d3.select(this)
-          .style('opacity', 1)
-          .style('stroke-width', '3px');
-    })
-    .on('mouseout', function() {
-        d3.selectAll('.line')
-          .style('opacity', 1)
-          .style('stroke-width', '2px');
-    });
-
-
+    .style("stroke", ([key, values]) => color(key));
+    
   // Draw legend
-  svg.selectAll(".country")
-    .data(dataByCountry)
-    .enter().append("text")
-      .attr("class", "legend")
-      .datum(function([key, values]) { return {id: key, value: values[values.length - 1]}; })
-      .attr("transform", function(d) { return "translate(" + xScale(d.value.year) + "," + yScale(d.value.gdp) + ")"; })
-      .attr("x", 3)
-      .attr("dy", "0.35em")
-      .style("font", "10px sans-serif")
-      .text(function(d) { return d.id; });
+  country.append("text")
+    .datum(([key, values]) => ({id: key, value: values[values.length - 1]}))
+    .attr("transform", ([key, value]) => "translate(" + xScale(value.year) + "," + yScale(value.gdp) + ")")
+    .attr("x", 3)
+    .attr("dy", "0.35em")
+    .style("font", "10px sans-serif")
+    .text(([key, value]) => key);
 });
 
 // Back button functionality
