1d15ec688dcecd76ef6a020dcdfcb73d25c3d0ad
jgulden2uiuc <140918950+jgulden2uiuc@users.noreply.github.com>
2023-07-30 10:47:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,88 +1,112 @@
-// Create the SVG canvas
-let svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+// main.js
+
+// Width and height of the chart
+const width = 960, height = 500;
+const margin = {top: 20, right: 80, bottom: 30, left: 50};
+
+// Create SVG
+const svg = d3.select('#chart').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .append('g')
+  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
 // Append a "Back" button but make it invisible by default
-let backButton = svg.append("text")
-    .attr("class", "backButton")
-    .attr("x", width - 50)
-    .attr("y", 20)
-    .style("font", "16px sans-serif")
-    .style("fill", "black")
-    .style("cursor", "pointer")
-    .style("visibility", "hidden")  // Hidden by default
-    .text("Back")
-    .on("click", function() {
-        // Hide the back button
-        d3.select(".backButton").style("visibility", "hidden");
-        // Reset the y-axis scale
-        y.domain(yDomain);
-        // Transition the y axis and line
-        transitionData(countries, data0);
-        // Return the line stroke and style to original state
-        d3.selectAll(".line").style("stroke", function(d) { return color(d.key); })
-        .style("stroke-width", "1.5px");
-        // Return the opacity of all lines back to 1
-        d3.selectAll(".line").style("opacity", 1);
-    });
+const backButton = svg.append("text")
+  .text("Back")
+  .attr("x", width - margin.right)
+  .attr("y", margin.top)
+  .style("font", "16px sans-serif")
+  .style("fill", "black")
+  .style("cursor", "pointer")
+  .style("display", "none")  // Hidden by default
+  .on("click", function() {
+    // Reload the page on click
+    location.reload();
+  });
 
-// Define the line
-let line = d3.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.population); });
+// Create Scales
+const xScale = d3.scaleLinear().range([0, width - margin.left - margin.right]);
+const yScale = d3.scaleLinear().range([height - margin.top - margin.bottom, 0]);
 
-// Create the line group
-let lines = svg.append("g")
-    .attr("class", "lines");
+// Create Axis
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yScale);
 
-// Create the line groups under the "lines" group
-let linesG = lines.selectAll(".lineGroup")
-    .data(countries)
+// Create Color Scale
+const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+// Create line generator
+const line = d3.line()
+  .x(function(d) { return xScale(d.year); })
+  .y(function(d) { return yScale(d.gdp); });
+
+// Load data
+d3.csv("API_NY.GDP.PCAP.KD_DS2_en_csv_v2_5728900.csv").then(function(data) {
+  // Prepare the data
+  const parsedData = [];
+  data.forEach(d => {
+    for (let i = 1960; i <= 2022; i++) {
+      parsedData.push({
+        country: d.country,
+        year: i,
+        gdp: +d[i]
+      });
+    }
+  });
+
+  // Group data by country
+  const dataByCountry = Array.from(d3.group(parsedData, d => d.country));
+  
+  color.domain(dataByCountry.map(([key, values]) => key));
+  
+  // Set domains for scales
+  xScale.domain(d3.extent(parsedData, function(d) { return d.year; }));
+  yScale.domain([0, d3.max(parsedData, function(d) { return d.gdp; })]);
+
+  // Draw axes
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + yScale.range()[0] + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  // Draw lines
+  const country = svg.selectAll(".country")
+    .data(dataByCountry)
     .enter().append("g")
-    .attr("class", "lineGroup");
+      .attr("class", "country");
 
-// Create the path, line, and fill under the line group
-let path = linesG.append("path")
+  country.append("path")
     .attr("class", "line")
-    .attr("d", function(d) { return line(d.values); })
-    .style("stroke", function(d) { return color(d.key); })
-    .style("stroke-width", "1.5px")
-    .on("mouseover", function(d) {
-        d3.selectAll(".line").style("opacity", 0.1);
-        d3.select(this).style("opacity", 1).style("stroke-width", "2.5px");
+    .attr("d", ([key, values]) => line(values))
+    .style("stroke", ([key, values]) => color(key))
+    .on("mouseover", function() {
+      d3.selectAll(".line").style("opacity", 0.2);
+      d3.select(this).style("opacity", 1).style("stroke-width", "2.5px");
     })
-    .on("mouseout", function(d) {
-        d3.selectAll(".line").style("opacity", 1);
-        d3.select(this).style("stroke-width", "1.5px");
+    .on("mouseout", function() {
+      d3.selectAll(".line").style("opacity", 1).style("stroke-width", "1.5px");
     })
-    .on("click", function(d) {
-        // Show the back button
-        d3.select(".backButton").style("visibility", "visible");
-        // Adjust the y axis scale
-        y.domain(d3.extent(d.values, function(c) { return c.population; }));
-        // Transition the y axis and line
-        transitionData(countries, d.values);
-        // Change the stroke of other lines to grey and current line to black
-        d3.selectAll(".line").style("stroke", "grey");
-        d3.select(this).style("stroke", "black");
+    .on("click", ([key, values]) => {
+      const selectedData = values;
+      yScale.domain(d3.extent(selectedData, d => d.gdp));
+      svg.select(".y.axis").transition().call(yAxis);
+      svg.selectAll(".line").attr("d", ([key, values]) => line(values));
+      backButton.style("display", "block");
     });
 
-// Creating the legend
-var legend = svg.selectAll('g')
-    .data(countries)
-    .enter()
-    .append('g')
-    .attr('class', 'legend');
-
-legend.append('rect')
-    .attr('x', width - 20)
-    .attr('y', function(d, i) { return i * 20; })
-    .attr('width', 10)
-    .attr('height', 10)
-    .style('fill', function(d) { return color(d.key); });
-
-legend.append('text')
-    .attr('x', width - 8)
-    .attr('y', function(d, i) { return (i * 20) + 9; })
-    .text(function(d) { return d.key; });
+  // Draw legend
+  country.append("text")
+    .datum(([key, values]) => ({country: key, value: values[values.length - 1]}))
+    .attr("transform", function(d) { 
+      return "translate(" + xScale(d.value.year) + "," + yScale(d.value.gdp) + ")"; 
+    })
+    .attr("x", 3)
+    .attr("dy", "0.35em")
+    .style("font", "10px sans-serif")
+    .text(d => d.country);
+});
